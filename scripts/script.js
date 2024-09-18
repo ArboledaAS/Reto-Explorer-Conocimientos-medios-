b0fea1656d20b5b8d544d40244bdc7c4693ed633
AndresArboleda <andresarb96@gmail.com>
2024-09-18 13:45:28 -0500

    Reto explorer - version 1.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,102 @@
 import { data } from "../data/data.js"
 
-let sizeData = data.length;
-let posicion = 0;
-let items = document.getElementById('items');
-let info;
-//let boton = document.getElementById('nombreElemento')
-let boton = document.getElementById('pestana');
-let btnAdelante = document.querySelector("#btnAdelante");
-let btnAtras = document.querySelector("#btnAtras");
+document.addEventListener('DOMContentLoaded', function(){
+    let posicion = JSON.parse(localStorage.getItem("pos")) || 0
 
-
-capturarDecrementar();
-capturarIncrementar();
-actualizar();
-
-
-
-
-
-
-
-function capturarDecrementar(){
-    //let elbotonatras = document.getElementById('btnAtras')
-    btnAtras.addEventListener('click',Decrementar);
-}
-
-function capturarIncrementar(){
-    //let elbotonadelante = document.getElementById('btnAdelante')
-    btnAdelante.addEventListener('click',Incrementar);
-}
-
-function Incrementar(){
+    let sizeData = data.length;
     
-    if(posicion < (sizeData-1)){
-        btnAtras.disabled = false
-        posicion = posicion + 1;
-        actualizar();
-    }else{
-        btnAdelante.disabled = true
+    let items = document.getElementById('items');
+    let info;
+    //let boton = document.getElementById('nombreElemento')
+    let boton = document.getElementById('pestana');
+    let btnAdelante = document.querySelector("#btnAdelante");
+    let btnAtras = document.querySelector("#btnAtras");
+    
+    
+    capturarDecrementar();
+    capturarIncrementar();
+    actualizar();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    function capturarDecrementar(){
+        //let elbotonatras = document.getElementById('btnAtras')
+        btnAtras.addEventListener('click',Decrementar);
     }
     
-}
-
-function Decrementar(){
-    if(posicion > 0){
-        btnAdelante.disabled = false
-        posicion = posicion - 1;
-        actualizar();
-    }else{
+    function capturarIncrementar(){
+        //let elbotonadelante = document.getElementById('btnAdelante')
+        btnAdelante.addEventListener('click',Incrementar);
+    }
+    
+    function Incrementar(){
         
-        btnAtras.disabled = true
+        if(posicion < (sizeData-1)){
+            btnAtras.disabled = false
+            posicion = posicion + 1;
+            actualizar();
+        }else{
+            btnAdelante.disabled = true
+        }
+        
     }
-}
-
-function actualizar(){
     
-    info = data[posicion]
+    function Decrementar(){
+        if(posicion > 0){
+            btnAdelante.disabled = false
+            posicion = posicion - 1;
+            actualizar();
+        }else{
+            
+            btnAtras.disabled = true
+        }
+    }
+    
+    function actualizar(){
 
-    items.innerHTML = ``
+        
+        
+        info = data[posicion]
 
-    boton.innerText = `${info.nombre}`
+        let objeto ={
+            nnom: info.nombre,
+            desc: info.descripcion,
+            img: info.img,
+            posi: posicion,
+        }
 
-    items.innerHTML = `<img src=${info.img} id="idimagen">`
-    localStorage.setItem('nombre', info.nombre);
-    localStorage.setItem('descrip', info.descripcion);
-    localStorage.setItem('img', info.img);
-    localStorage.setItem('posicion', info.img);
-} 
-
-
-
+        localStorage.setItem("clash", JSON.stringify(objeto));
+    
+        items.innerHTML = ``
+    
+        boton.innerText = `${info.nombre}`
+    
+        items.innerHTML = `<img src=${info.img} id="idimagen">`
+    //     localStorage.setItem('nombre', info.nombre);
+    //     localStorage.setItem('descrip', info.descripcion);
+    //     localStorage.setItem('img', info.img);
+    //     localStorage.setItem('posicion', posicion);
+    // 
+    } 
+    
+    
+    // function presente(){
+    //     actualizar
+    //     localStorage.setItem('nombre', info.nombre);
+    //     localStorage.setItem('descrip', info.descripcion);
+    //     localStorage.setItem('img', info.img);
+    //     localStorage.setItem('posicion', posicion);
+    
+    //     cargar;
+    // }
+    
+    
+    
+})
 
--- scripts/scriptinfo.js
+++ scripts/scriptinfo.js
@@ -1,16 +1,24 @@
 let imagen = document.getElementById('items');
 let informacion = document.getElementById('texto');
-let boton = document.querySelector("#btnVolver");
+let boton = document.getElementById("btnVolver");
 
+let personaje = JSON.parse(localStorage.getItem("clash")) || []
 
 
 cargarInformacion();
 
+boton.addEventListener('click',function(){
+    location.href = "../index.html";
+    localStorage.setItem('pos', personaje.posi)
+
+
+})
 
 function cargarInformacion(){
+    
 
-    imagen.innerHTML = `<img src=${localStorage.getItem('img')} id="idimagen">`;
-    informacion.innerText = `${localStorage.getItem('descrip')}`;
+    imagen.innerHTML = `<img src=${personaje.img} id="idimagen">`;
+    informacion.innerText = `${personaje.desc}`;
 }
 
 
